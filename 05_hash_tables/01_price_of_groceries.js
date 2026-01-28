3abde43063143389318967d25dd849896dca4270
Gabriel Padilha <gabriel.padilha1204@gmail.com>
2026-01-28 12:44:38 -0300

    simple example of hash table

---
+++ 05_hash_tables/01_price_of_groceries.js
@@ -0,0 +1,14 @@
+"use strict"
+
+const book = {
+  "apple" : 0.67,
+  "grapes" : 1.20
+}
+
+// Same result
+// const book = {}
+
+// book["apple"] = 0.67
+// book["grapes"] = 1.28
+
+console.log(book["grapes"])
